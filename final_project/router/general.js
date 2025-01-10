3ca5baddcc0cf23dca8c7c0ee885779b6b461160
MasatoSotoma <masatosotoma@gmail.com>
2025-01-09 23:35:41 -0500

    Getting book details func added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,10 +21,17 @@ public_users.get("/", async (req, res) => {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get("/author/:author", async (req, res) => {
+  // get array of matching book objects
+  const matchingBooks = Object.values(await books).filter(
+    (book) => book.author.toLowerCase() === req.params.author.toLowerCase()
+  );
+  if (matchingBooks.length > 0) {
+    return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+  } else {
+    return res.status(404).json({ message: "No books by that author." });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
